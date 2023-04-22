a325ec8f77b0a466a74bbc5e779f5a34b99fe75f
prashant <prashantdsk75@gmail.com>
2023-04-22 17:11:12 +0530

    change

--- client/src/customHooks/useSetupHook.js
+++ client/src/customHooks/useSetupHook.js
@@ -1,15 +1,10 @@
-import axios from "axios";
 import socket from "socketConnection/socketConnection.js";
 import { useEffect } from "react";
 
-const useSocketHook = (user) => {
+const useSocketHook = () => {
     useEffect(() => {
         socket.connect();
-        socket.on("server", (msg) => {
-            console.log(msg);
-        });
-        socket.emit("client", "fork");
-    }, [user]);
+    }, []);
 };
 
 export default useSocketHook;
